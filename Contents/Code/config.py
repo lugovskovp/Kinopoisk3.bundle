563e78deb8b001e2cc3c3bd6fa8fb696373e3f88
Paul Lugovskov <p.lugovskov@gmail.com>
2025-07-12 17:24:15 +0300

    fix: В лог - сколько осталось попыток на сегодня

--- Contents/Code/config.py
+++ Contents/Code/config.py
@@ -103,10 +103,11 @@ def APItokenRemains():
       remains = 1000
     else:
       if used >= dailyQuota:
+        Log(u"APItokenRemains::exceeded quota:%s used:%s\n" % (dailyQuota, used))                   # type: ignore
         return False    #You exceeded the quota. You have sent 517 request, but available 500 per day
       else:
         remains = dailyQuota - used
-    Log(u"APItokenRemains:: accountType:%s dailyQuota:%s used:%s\n" % (accountType, dailyQuota, used))                  # type: ignore
+    Log(u"APItokenRemains:: accountType:%s remains:%s\n" % (accountType, remains))                  # type: ignore
   return remains
 
      
