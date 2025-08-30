47b76a6ddaa27fbc6f60af505e7feaace5794f8c
Paul Lugovskov <p.lugovskov@gmail.com>
2025-08-30 17:43:15 +0300

    feat: добавлена настройка Поиск: для сериалов убирать из результатов F:фильм и V:видео done #49

--- Contents/Code/config.py
+++ Contents/Code/config.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 # coding=utf-8
 
-import requests             # [а к нему еще chardet, urllib3, certifi, idna]      # type: ignore 
-
 # константы ===============================================================================
 
-NAME = u'Кинопоиск3' # % VER
+NAME = u'Кинопоиск3' 
 VER = '0.0.1'
 PREFIX = '/video/kino'
-TITLE = 'Агент Кинопоиск Unofficial'
+TITLE = u'Агент Кинопоиск Unofficial'
 ART = 'art-default.png'
 ICON = 'icon-default.png'
-
-version_path = Core.storage.join_path(Core.bundle_path, 'Contents', 'VERSION') # type: ignore
-if Core.storage.file_exists(version_path):                                    # type: ignore
-  str_version = Core.storage.load(version_path)                               # type: ignore
-  VER = str_version.split()[0]
 LANGUAGES = [Locale.Language.Russian, Locale.Language.English, Locale.Language.NoLanguage]  # type: ignore # [Locale.Language.Russian, Locale.Language.English, Locale.Language.NoLanguage,]
+# значение версии взять из файла VERSION
+version_path = Core.storage.join_path(Core.bundle_path, 'Contents', 'VERSION') # type: ignore
+if Core.storage.file_exists(version_path):                                     # type: ignore
+  str_version = Core.storage.load(version_path)                                # type: ignore
+  VER = str_version.split()[0]
 
-REQUEST_QTY_SEARCH_MIN = 20   # при поиске должно оставаться не менее
+REQUEST_QTY_SEARCH_MIN = 20   # при старте поиска должно оставаться не менее Х запросов
 
 # Update vars
 UPDATER_REPO = 'lugovskovp'
@@ -27,7 +25,7 @@ UPDATER_BETA_URL = 'https://api.github.com/repos/%s/Kinopoisk3.bundle/tags?per_p
 UPDATER_ARCHIVE_URL = 'https://github.com/%s/Kinopoisk3.bundle/archive/refs/tags/'
 UPDATE_INTERVAL_MIN = 10    # MINIMAL INTERVAL = 10min
 
-# URLS      FILM_xxxx - для функций получения инфо с кинопоиска для update
+# URLS      FILM_xxxx - для функций получения инфо с кинопоиска для update и поиска
 API_BASE_URL      = 'https://kinopoiskapiunofficial.tech'
 KEYWORD_SEARCH    = '/api/v2.1/films/search-by-keyword?keyword=%s&page=%s'
 FILM_DETAILS      = '/api/v2.2/films/%s'
@@ -38,11 +36,11 @@ FILM_REVIEW       = '/api/v2.2/films/%s/reviews?page=1&order=DATE_DESC'
 SERIAL_SEASONS    = '/api/v2.2/films/%s/seasons'
 
 # scoring
-UNKNOWN_YEAR = 1900
-MAX_VALID_YEAR  = 2035
-SCORE_WEIGHT_NAME = 80
-SCORE_WEIGHT_YEAR = 15  # 100 - SCORE_WEIGHT_NAME
-SCORE_WEIGH_JANRE = 5
+UNKNOWN_YEAR = 1900     # значение неизвестного года
+MAX_VALID_YEAR  = 2035  # максимально допустимый год производства
+SCORE_WEIGHT_NAME = 80  # % веса имени - при полном совпадении с найденным - 80
+SCORE_WEIGHT_YEAR = 15  # % веса года
+SCORE_WEIGH_JANRE = 5   # % веса типа фильма (для фильмов и сериалов в [ 'FILM', 'VIDEO', 'TV_SERIES', 'MINI_SERIES', 'TV_SHOW' ])
 
 
 
