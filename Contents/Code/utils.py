a5e6b97e55d7dac54458158c6dd77d3d8e168c96
Paul Lugovskov <p.lugovskov@gmail.com>
2025-12-02 20:31:49 +0300

    fix: #50 доработана работа с json в поиске

--- Contents/Code/utils.py
+++ Contents/Code/utils.py
@@ -3,7 +3,6 @@
 
 # find in \Contents\Library\Shared
 import requests             # [а к нему еще chardet, urllib3, certifi, idna]      # type: ignore 
-import traceback            # для get_json()
 import re
 
 from config import API_BASE_URL
@@ -21,6 +20,39 @@ def AppendSearchResult(results, id, name=None, year=-1, score=0, lang=None):
   return
 
 # -------------------------------------------    
+def get_json(url):
+  ''' Полоучить json 
+  
+  url - URL с endpoint и параметрами 
+  '''
+  ApiKey = Prefs['api_key']   # type: ignore
+  headers = {
+      'Accept': 'application/json',
+      'X-API-KEY': ApiKey,              
+      'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)'
+      }
+  try:
+    response = requests.get(url, headers=headers)  # data = r.content  # Content of response
+  except requests.ConnectionError as e:
+    # TODO ERR_CONNECTION_TIMED_OUT - get away?
+    Core.log.error("OOPS!! Connection Error. Или Вы не в интернете, или http://kinopoiskapiunofficial.tech вне доступа.\n") # type: ignore
+    Core.log.error(str(e))  # type: ignore   
+    return False
+  except:
+    Core.log.error("Какая-то ошибка, но не Connection Error.")  # type: ignore
+    return False
+  rj = response.json()
+  if not (rj or rj.get('message') or response.status_code != '200'):
+    # что-то пошло не так
+    #status_code == 401:    You don't have permissions There are not valid token  
+    #status_code == 402:    You exceeded the quota.
+    #status_code == 429:    Rate limit exceeded
+    Core.log.error("Не, ну точно ошибка %s: %s" % (response.status_code, rj.get('message')))  # type: ignore
+    Core.log.error("APItokenRemains::ERROR %s: %s" % (status_code, response.json().get("message")))    # type: ignore
+    return False
+  return rj
+
+
 def APItokenRemains():
   '''
   Return False if token err happened
@@ -28,49 +60,26 @@ def APItokenRemains():
   #/api/v1/api_keys/{apiKey}  #получить данные об api key 
   '''
   valid = False
-  key = Prefs['api_key']                                                                # type: ignore
-  url = '%s/api/v1/api_keys/%s' % (API_BASE_URL, key)
-  headers={
-    'Accept': 'application/json',
-    'X-API-KEY': key, # type: ignore
-    'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)'
-    }
-  try:
-    response = requests.get(url, headers=headers)  # data = r.content  # Content of response
-  except requests.ConnectionError as e:
-    # TODO ERR_CONNECTION_TIMED_OUT - get away?
-    w("OOPS!! Connection Error. Или Вы не в интернете, или http://kinopoiskapiunofficial.tech вне доступа.\n")
-    w(str(e))  
+  url = '%s/api/v1/api_keys/%s' % (API_BASE_URL, Prefs['api_key'] )  # type: ignore
+  json = get_json(url)
+  BIG_VALUE = 1000
+  dq = json.get("dailyQuota")
+  if not dq:
+    w(u"APItokenRemains:: wrwre are not daylyQuoya in json")      
     return False
-  except:
-    return False
-    pass
-  status_code = response.status_code
-  d(u"APItokenRemains::response code:%s json:%s" % (status_code, response.json()))                  # type: ignore
-  if status_code != 200:
-    # что-то пошло не так
-    #status_code == 401:    You don't have permissions There are not valid token  
-    #status_code == 402:    You exceeded the quota.
-    #status_code == 429:    Rate limit exceeded
-    w("APItokenRemains::ERROR %s: %s" % (status_code, response.json()["message"]))                  # type: ignore
-    return False
+  value = dq.get("value")
+  used  = dq.get("used")
+  w(u"APItokenRemains::dailyQuota value:%s used:%s" % (value, used)) 
+  accountType = json.get("accountType") or 'unknown account type'
+  if accountType == 'UNLIMITED':
+    remains = BIG_VALUE
   else:
-    # 200, ключ есть, ответ есть. Может, даже остались в квоте попытки.
-    json = response.json()
-    used = 0
-    dailyQuota = 0
-    accountType = json["accountType"]
-    dailyQuota = json["dailyQuota"]["value"]
-    used = json["dailyQuota"]["used"]
-    if accountType == 'UNLIMITED':
-      remains = 1000
+    if used >= value:
+      w(u"APItokenRemains::exceeded dailyQuota value:%s used:%s" % (value, used))                   # type: ignore
+      return False    #You exceeded the quota. You have sent 517 request, but available 500 per day
     else:
-      if used >= dailyQuota:
-        w(u"APItokenRemains::exceeded quota:%s used:%s" % (dailyQuota, used))                   # type: ignore
-        return False    #You exceeded the quota. You have sent 517 request, but available 500 per day
-      else:
-        remains = dailyQuota - used
-    Log(u"APItokenRemains:: accountType:%s remains:%s" % (accountType, remains))                  # type: ignore
+      remains = value - used
+  Log(u"APItokenRemains:: accountType:%s remains:%s" % (accountType, remains))                  # type: ignore
   return remains
 
 
@@ -102,24 +111,6 @@ def getGUIDs(guid):
   if imdbSearch and imdbSearch.group(1):
     guids['imdbId'] = imdbSearch.group(1)  
   return guids
-
-
-def get_json(url):
-  headers={
-      'Accept': 'application/json',
-      'X-API-KEY': Prefs['api_key'], # type: ignore
-      'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)'
-      }
-  try:
-    rj = requests.get(url, headers=headers).json()   # data = r.content  # Content of response
-  except:
-    Log("\n\n err::Except in get_json - requests.get(url=%s)" % url)      # type: ignore
-    Log(traceback.format_exc())                                           # type: ignore
-    return                 # в случае ошибки, вернуть пустую строку
-  if 'message' in rj:    # признак ошибки
-    Log("\n\n err::Попытка поиска без ключа: %s" % rj['message'])         # type: ignore
-    return rj
-  return rj
 
 
 def resurrectMetadataId(id):
